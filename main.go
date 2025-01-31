33c552469ff6577dfd33f2411ff1ba2bf2b851b1
cooler-SAI <cooler19860212@yahoo.com>
2025-01-31 20:58:10 +0300

    set up logger

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Hello counter")
+}
